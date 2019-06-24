4e3a76366e02eb34d30cd79aefc56c739b0f24e6
Andy Casey <andycasey@gmail.com>
2019-06-24 10:26:45 +1000

    update script to add required parameters

--- data/prepare.py
+++ data/prepare.py
@@ -20,21 +20,23 @@ ignore_parameters = [
     # kill your CPU if you don't ignore it.
     "phot_variable_flag", 
     "rv_single_epoch_variance",
+    "rv_single_epoch_scatter",
+    "astrometric_unit_weight_error",
     "p_sb_16",
     "p_sb_50",
     "p_sb_84",
+    "absolute_g_mag",
+    "absolute_rp_mag",
+    "absolute_bp_mag",
+    "phot_bp_variability",
+    "phot_rp_variability",
+    "phot_g_variability",
+    "rv_single_epoch_scatter",
+    "astrometric_unit_weight_error",
 ]
 
 if False:
     ignore_parameters.extend([
-        "absolute_g_mag",
-        "absolute_rp_mag",
-        "absolute_bp_mag",
-        "phot_bp_variability",
-        "phot_rp_variability",
-        "phot_g_variability",
-        "rv_single_epoch_scatter",
-        "astrometric_unit_weight_error",
     ])
 
 print(f"Ignoring parameters: {','.join(ignore_parameters)}")
@@ -45,10 +47,22 @@ with h5.File(output_path, "w") as h:
 
     with fits.open(input_path) as image:
         for parameter_name in tqdm(image[hdu].columns.names):
-            #for i, parameter_name in enumerate(image[hdu].columns.names):
-            print(parameter_name)
             if parameter_name not in ignore_parameters: 
                 group.create_dataset(parameter_name, data=image[hdu].data[parameter_name])
 
+        # Create additional entries.
+        group.create_dataset("ruwe",
+                             data=np.sqrt(group["astrometric_chi2_al"][()]/(group["astrometric_n_good_obs_al"][()] - 5)))
+
+        group.create_dataset("rv_jitter",
+                             data=group["radial_velocity_error"][()]/np.sqrt(group["rv_nb_transits"][()] * 0.5 * np.pi))
+
+        mu = 5 * np.log10(group["parallax"][()]/100.0)
+        S = np.sqrt(group["astrometric_n_good_obs_al"][()])
+
+        for band in ("g", "bp", "rp"):
+            group.create_dataset(f"absolute_{band}_mag", data=group[f"phot_{band}_mean_mag"][()] + mu)
+            group.create_dataset(f"phot_{band}_variability",
+                                 data=S * group[f"phot_{band}_mean_flux"][()] / group[f"phot_{band}_mean_flux_error"][()])
 
 print(f"Created {output_path}")
